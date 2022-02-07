1667f80987db3fdcdd63286463828dffe9a2b903
Sam Schlinkert <sschlinkert@gmail.com>
2022-02-06 23:29:27 -0500

    switches arg parser from structopt to clap

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
-extern crate structopt;
+use clap::Parser;
 use nametag::*;
 use std::path::PathBuf;
-use structopt::StructOpt;
 
-/// nametag: Generate random usernames
-#[derive(StructOpt, Debug)]
-#[structopt(name = "nametag")]
-struct Opt {
+/// Generate random usernames
+#[derive(Parser, Debug)]
+#[clap(version, name = "nametag")]
+struct Args {
     /// Prints verbose output, including parameters as received
-    #[structopt(short = "v", long = "verbose")]
+    #[clap(short = 'v', long = "verbose")]
     verbose: bool,
 
-    /// Provide a txt file with a list of words to generate username from randomly
-    #[structopt(short = "l", long = "list")]
+    /// Provide a text file with a list of words to generate username from randomly
+    #[clap(short = 'l', long = "list")]
     list_file_path: Option<PathBuf>,
 
     /// Set how many random usernames to output
-    #[structopt(short = "n", long = "number", default_value = "10")]
+    #[clap(short = 'n', long = "number", default_value = "10")]
     number_to_print: usize,
 
     /// Set maximum username length. Must be greater than 5.
-    #[structopt(short = "m", long = "maximum", default_value = "50")]
+    #[clap(short = 'm', long = "maximum", default_value = "50")]
     maximum_length: u8,
 
     /// Uses Title Case for words in generated usernames
-    #[structopt(short = "t", long = "title-case")]
+    #[clap(short = 't', long = "title-case")]
     title_case: bool,
 }
 
 fn main() {
-    let opt = Opt::from_args();
+    let opt = Args::parse();
     if opt.verbose {
-        println!("Received options: {:?}", opt);
+        eprintln!("Received options: {:?}", opt);
         match &opt.list_file_path {
-            Some(file_path) => println!("Will use word list at {:?}", file_path),
-            None => println!("Will use default word list"),
+            Some(file_path) => eprintln!("Will use word list at {:?}", file_path),
+            None => eprintln!("Will use default word list"),
         }
     }
     if opt.maximum_length < 5 {
